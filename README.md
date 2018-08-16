36d222161955375849b45f4470bd28b4eb69246d
yoh <kawasaki.liamg@gmail.com>
2018-08-17 00:26:29 +0900

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cpp-longest-identifier
-find longest identifier in C++ Standard
+Find longest identifier in C++ Standard.
+
+```
+$ ./cpplongestid.py
+atomic_compare_exchange_strong_explicit 39
+hardware_constructive_interference_size 39
+propagate_on_container_move_assignment 38
+propagate_on_container_copy_assignment 38
+hardware_destructive_interference_size 38
+atomic_compare_exchange_weak_explicit 37
+select_on_container_copy_construction 37
+uninitialized_default_construct_n 33
+atomic_flag_test_and_set_explicit 33
+...
+```
+
+## License
+MIT License
---
+++ cpplongestid.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+cpplongestid.py -- find longest identifier in C++ Standard
+
+Copyright (c) 2018 yoh
+"""
+import os
+import re
+
+LENGTH_LIMIT = 25
+
+
+def parse_subtex(basedir, texfile):
+    identifers = set()
+    with open(os.path.join(basedir, texfile), 'r') as f:
+        in_itemdecl = False
+        for line in f:
+            if re.match(r'\\begin\{itemdecl\}', line):
+                in_itemdecl = True
+            elif re.match(r'\\end\{itemdecl\}', line):
+                in_itemdecl = False
+            elif in_itemdecl:
+                m = re.findall(r'[_a-z][_a-z0-9]+', line)
+                if m:
+                    identifers.update(m)
+    return [s for s in identifers if len(s) > LENGTH_LIMIT]
+
+
+def parse_roottex(basedir, texfile):
+    identifers = set()
+    with open(os.path.join(basedir, texfile), 'r') as f:
+        for line in f:
+            m = re.match(r'\\include\{([a-z]+)\}', line)
+            if not m:
+                continue
+            subtex = m.group(1) + '.tex'
+            ids = parse_subtex(basedir, subtex)
+            identifers.update(ids)
+    identifers = sorted(list(identifers), key=lambda s: -len(s))
+    for name in identifers:
+        print(name, len(name))
+
+
+if __name__ == '__main__':
+    parse_roottex('cppdraft/source', 'std.tex')
